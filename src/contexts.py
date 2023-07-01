11309d4849e8721c29415a394f627da586435e52
TheGregggg <gregoirelayet@gmail.com>
2023-07-01 18:59:12 +0200

    page working

--- src/contexts.py
+++ src/contexts.py
@@ -10,10 +10,12 @@ CONTEXTS = {
         'name':'Grashboard',
         'subtitle': 'グレッグ',
         'search_url':'https://www.google.com/search?q=',
+        'elem_per_col': 4,
         'applications':[
             Application('GregNAS', '#0067E6', '#fff', 'http://gregnas:5000/'),
             Application('GregFlix', '#E50914', '#fff', 'http://gregflix/'),
             Application('GregRouter', '#eb7d34', '#fff', 'https://10.69.0.1/'),
+            Application('Actual', '#874ad5', '#fff', 'http://192.168.1.69:5006/'),
         ]
     }
 }
